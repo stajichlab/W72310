e98b2ce135ffd44dd8465fc295adef8a71a583fc
Lotus Lofgren <LotusLofgren@gmail.com>
2020-10-21 14:18:48 -0700

    Create 00_init.sh

---
+++ pipeline/00_init.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+module load samtools/1.9
+module load bwa/0.7.17
+module load picard
+if [ -f config.txt ]; then
+	source config.txt
+fi
+pushd genome
+RELEASE=39
+SPECIES=AfumigatusAf293
+URL=https://fungidb.org/common/downloads/release-${RELEASE}/$SPECIES
+PREF=FungiDB-${RELEASE}_${SPECIES}
+echo "expected $REFGENOME"
+FASTAFILE=${PREF}_Genome.fasta
+DOMAINFILE=${PREF}_InterproDomains.txt
+GFF=${PREF}.gff
+echo "working off $FASTAFILE - check if these don't match may need to update config/init script"
+
+if [ ! -f $DOMAINFILE ]; then
+	curl -O $URL/txt/$DOMAINFILE
+fi
+if [ ! -f $FASTAFILE ] ; then
+	curl -O $URL/fasta/data/$FASTAFILE
+fi
+if [ ! -f $GFF ]; then
+	curl -O $URL/gff/data/$GFF
+fi
+
+if [[ ! -f $FASTAFILE.fai || $FASTAFILE -nt $FASTAFILE.fai ]]; then
+	samtools faidx $FASTAFILE
+fi
+if [[ ! -f $FASTAFILE.bwt || $FASTAFILE -nt $FASTAFILE.bwt ]]; then
+	bwa index $FASTAFILE
+fi
+
+DICT=$(basename $FASTAFILE .fasta)".dict"
+
+if [[ ! -f $DICT || $FASTAFILE -nt $DICT ]]; then
+	rm -f $DICT
+	picard CreateSequenceDictionary R=$FASTAFILE O=$DICT
+	ln -s $DICT $FASTAFILE.dict 
+fi
+
+popd
